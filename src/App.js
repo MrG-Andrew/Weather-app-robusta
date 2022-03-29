4c7d98a30ad3e488502a1321a10c664a0bdc20fc
andrew <alm3allmandy@gmail.com>
2022-03-29 23:14:48 +0200

    finished ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import './App.css';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
-import Home from './components/Home'
+
+const api = {
+  key: "&appid=c9b67fcbf1ac1bff6e1860ef9e260888&units=metric",
+  base: "https://api.openweathermap.org/data/2.5/weather?q="
+}
 
 
-function BooksApp() {
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let day = new Date().getDay();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let month = new Date().getMonth();
+
+let newDate =  days[day]+",  "+new Date().getDate()+' '+months[month]+' '+ new Date().getFullYear();
+
+
+function App() {
   
 
   return (
     
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={
-            <Home/>
-          }/>
-        </Routes>
-        
-      </Router>
+    <div className="app cold">
+      <main>
+        <div className="search-box">
+          <input type="text" placeholder='Search...' id="search-bar" />
+        </div>
+        <div className='location-box'>
+          <div className="location">Cairo, egy</div>
+          <div className="date"><em>{newDate}</em></div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°C</div>
+          <div className="weather">
+            Sunny
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
 
-export default BooksApp;
+export default App;
